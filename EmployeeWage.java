1706dddd2ad60d9d7c401eedd2fa31be9bedf4ae
shindeanant <shindeanant1997@gmail.com>
2021-09-21 12:20:14 +0530

    Calculate Daily Employee Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -6,6 +6,12 @@ public class EmployeeWage {
 
 		System.out.println("Welcome to Employee Wage Computation Program");
 
+		int WAGE_HOURS = 20;
+
+		int DAILY_HOURS = 8;
+
+		int DAILY_EMP_WAGE;
+
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 
 		System.out.println(empCheck);
@@ -13,12 +19,17 @@ public class EmployeeWage {
 		if (empCheck == 1) {
 
 			System.out.println("Employee is Present");
+
+			System.out.println("Daily Emp Wage is :" + WAGE_HOURS * DAILY_HOURS);
+
 		}
 
 		else {
 			System.out.println("Employee is Absent");
 
 		}
+
+		DAILY_EMP_WAGE = WAGE_HOURS * DAILY_HOURS;
 
 	}
 }
