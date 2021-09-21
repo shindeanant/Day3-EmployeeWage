e5c4be76a2f9fd7a051f97f3e3369fb90ca3f5c9
shindeanant <shindeanant1997@gmail.com>
2021-09-21 12:08:55 +0530

    Emp Attendance Chcek

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,10 +3,22 @@ package com.eployeewage;
 public class EmployeeWage {
 
 	public static void main(String[] args) {
-		
-		
+
 		System.out.println("Welcome to Employee Wage Computation Program");
 
-	}
+		double empCheck = Math.floor(Math.random() * 10) % 2;
 
+		System.out.println(empCheck);
+
+		if (empCheck == 1) {
+
+			System.out.println("Employee is Present");
+		}
+
+		else {
+			System.out.println("Employee is Absent");
+
+		}
+
+	}
 }
