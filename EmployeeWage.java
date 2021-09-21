b2ad2a3d2bb1dd08fbd5ce4d534542e4d6fe576c
shindeanant <shindeanant1997@gmail.com>
2021-09-21 12:03:03 +0530

    Welcome to Employee wage computation problem

---
+++ EmployeeWage.java
@@ -0,0 +1,12 @@
+package com.eployeewage;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Welcome to Employee Wage Computation Program");
+
+	}
+
+}
