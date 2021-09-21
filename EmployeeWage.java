785e031b78c5b8b5979cea6105b638a68308de62
shindeanant <shindeanant1997@gmail.com>
2021-09-21 12:41:04 +0530

    Calculate Monthly Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -6,29 +6,17 @@ public class EmployeeWage {
 
 		System.out.println("Welcome to Employee Wage Computation Program");
 
-		int empHr;
+		int DAYS_PER_MONTH = 20;
+		int WAGE_HOURS = 20;
+		int DAILY_HOURS = 8;
 
-		int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+		int DAILY_WAGES = WAGE_HOURS * DAILY_HOURS;
 
-		System.out.println("Random Integers: " + empCheck);
+		System.out.println("Employee daily wage is :" + DAILY_WAGES);
 
-		switch (empCheck) {
-		case 1:
-			empHr = 8;
-			System.out.print("full time");
-			break;
-		case 2:
-			empHr = 4;
-			System.out.print("part time");
-			break;
-		case 0:
-			empHr = 0;
-			System.out.println("absent");
-			break;
+		int Monthly_Wage = DAILY_WAGES * DAYS_PER_MONTH;
 
-		default:
-			empHr = 0;
-			break;
-		}
+		System.out.println("Monthly wage of employee is:" + Monthly_Wage);
+
 	}
 }
