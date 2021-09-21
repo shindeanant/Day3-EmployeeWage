8ad2e262a08ed86d36f21db1db91c582629611bf
shindeanant <shindeanant1997@gmail.com>
2021-09-21 12:45:44 +0530

    Calculate Wages till a condition of total working hours or days is reached for a month - Assume 100 hours and 20 days

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -6,17 +6,52 @@ public class EmployeeWage {
 
 		System.out.println("Welcome to Employee Wage Computation Program");
 
-		int DAYS_PER_MONTH = 20;
-		int WAGE_HOURS = 20;
-		int DAILY_HOURS = 8;
+		int Total_hurs = 100;
+		int Total_days = 20;
+		int wage_hurs = 20;
+		int daily_hurs = 8;
 
-		int DAILY_WAGES = WAGE_HOURS * DAILY_HOURS;
+		int empHr;
+		int empWage = 0;
+		int totalEmpWage = 0;
+		int totalWorkingDays = 0;
+		int totalEmpHrs = 0;
 
-		System.out.println("Employee daily wage is :" + DAILY_WAGES);
+		while (totalEmpHrs <= Total_hurs && totalWorkingDays < Total_days) {
 
-		int Monthly_Wage = DAILY_WAGES * DAYS_PER_MONTH;
+			totalWorkingDays++;
 
-		System.out.println("Monthly wage of employee is:" + Monthly_Wage);
+			int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+
+			System.out.println("Random Integers: " + empCheck);
+
+			switch (empCheck) {
+			case 1:
+				empHr = 8;
+				System.out.print("full time");
+				break;
+			case 2:
+				empHr = 4;
+				System.out.print("part time");
+				break;
+			case 0:
+				empHr = 0;
+				System.out.println("absent");
+				break;
+
+			default:
+				empHr = 0;
+				break;
+			}
+
+			empWage = empHr * wage_hurs;
+
+			totalEmpWage = totalEmpWage + empWage;
+			System.out.println("days" + totalWorkingDays + "emphrs" + empHr);
+			System.out.println("wage" + totalEmpWage);
+			System.out.println("total wage" + totalEmpWage);
+
+		}
 
 	}
 }
